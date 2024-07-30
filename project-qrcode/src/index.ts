40c2e3f274354d54c39b91769ea6bbbf84138b68
Rhuan Kowic Santos <rhuanksantos2@gmail.com>
2024-07-29 23:41:46 -0300

    FInalizando conversao.

--- project-qrcode/src/index.ts
+++ project-qrcode/src/index.ts
@@ -4,12 +4,12 @@ import createQRCode from "./services/qr-code/create.js";
 import createPassword from "./services/password/create.js";
 
 async function main() {
-  prompt.get(mainPrompt, async (err, res) => {
+  prompt.get(mainPrompt, async (err, res: any) => {
     if (err) {
       console.error(err);
       return;
     }
-    const select = isNaN(Number(res.select)) ? 0 : parseInt(res.select);
+    const select = isNaN(Number(res.select)) ? 0 : parseInt(res.select, 10);
     if (select === 1) await createQRCode();
     if (select === 2) await createPassword();
   });
